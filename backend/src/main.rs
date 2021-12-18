57a2c2ee0fccd3fbb09fc16bfdfae1afb5a08343
hihumikan <musecharo@gmail.com>
2021-12-19 03:38:03 +0900

    :seedling:  不要物の削除の途中経過

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, Error, HttpResponse, HttpServer};
+
+async fn index() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body("Hello, world!"))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
